57994f64c3d8ba6f481be7517b04e63d0202c77a
DUIT <132730091+jsum01@users.noreply.github.com>
2025-05-14 00:56:50 +0900

    [Silver IV] Title: 수 찾기, Time: 476 ms, Memory: 46480 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,70 +1,28 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-/**
- * 문제 설명
- * 1. 첫째 줄에 자연수 N 입력(1 <= N <= 100,000)
- * 2. 둘째 줄에 N개의 정수 입력.
- * 3. 셋째 줄에 자연수 M 입력(1 <= N <= 100,000)
- * 4. 넷째 줄에 M개의 정수 입력
- * 넷째 줄에 입력한 수들이 둘째 줄에 입력한 수 중 존재하는지 알아낸다.
- * 존재하면 1, 존재하지 않으면 0을 출력
- *
- * [조건]
- * 시간제한: 1초
- * 메모리 제한: 128MB
- * 정수 크기: 모든 정수의 크기는 +-32bit 여야 한다.
- */
-public class Main {
-    static Scanner sc = new Scanner(System.in);
-    static int N, M;
-    static List<Integer> Nlist = new ArrayList<>();
-    static List<Integer> Mlist = new ArrayList<>();
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
-        input();
+        int N = Integer.parseInt(br.readLine());
+        String[] Ns = br.readLine().split(" ");
+        Set<String> Nset = new HashSet<>(Arrays.asList(Ns)); // HashSet 사용
 
-        Collections.sort(Nlist);
+        int M = Integer.parseInt(br.readLine());
+        String[] Ms = br.readLine().split(" ");
 
-        for(int i = 0; i < Mlist.size(); i++) {
-            int key = Mlist.get(i);
-            if(binarySearch(Nlist, key)) { // 찾았을 경우
-                System.out.println(1);
+        for (int i = 0; i < M; i++) {
+            if (Nset.contains(Ms[i])) {
+                bw.write("1");
             } else {
-                System.out.println(0);
+                bw.write("0");
             }
+            bw.write("\n");
         }
-    }
 
-    private static void input() {
-        N = sc.nextInt();
-        for (int i = 0; i < N; i++) { // N번 입력
-            Nlist.add(sc.nextInt());
-        }
-        M = sc.nextInt();
-        for (int i = 0; i < M; i++) { // M번 입력
-            Mlist.add(sc.nextInt());
-        }
-    }
-
-    private static boolean binarySearch(List<Integer> arr, int key) {
-        int start = 0;
-        int end = arr.size() - 1;
-        boolean isFind = false;
-
-        while(start <= end) {
-            int mid = (start + end) / 2;
-
-            if(key > arr.get(mid)) { // 찾는 값이 중간 값보다 큰 경우
-                start = mid + 1;
-            } else if(key < arr.get(mid)) { // 찾는 값이 중간 값보다 작은 경우
-                end = mid - 1;
-            } else { // 찾았다!
-                return true;
-            }
-        }
-        return false;
+        bw.flush();
+        bw.close();
     }
 }
